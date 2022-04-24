aaaf01273c2eac65b18f87ea7bb8d6fa29897c42
Anshul Kamath <anshulkam@gmail.com>
2022-04-24 02:02:40 -0700

    implemented point_equals

--- tests/test-ec.c
+++ tests/test-ec.c
@@ -36,9 +36,27 @@ void test_point_create() {
     log_tests(tester);
 }
 
+
+void test_point_equals() {
+    testing_logger_t *tester = create_tester();
+    point_t p1, p2;
+
+    // should be equal
+    point_create(&p1, "0x12345678", "0x87654321");
+    point_create(&p2, "0x12345678", "0x87654321");
+    expect(tester, point_equals(&p1, &p2));
+    
+    // should not be equal
+    point_create(&p2, "0x87654321", "0x12345678");
+    expect(tester, !point_equals(&p1, &p2));
+
+    log_tests(tester);
+}
+
 int main() {
     test_point_init();
     test_point_create();
+    test_point_equals();
 
     return 0;
 }
