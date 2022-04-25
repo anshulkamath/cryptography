55377fae7d7f44f2199d158ee8c4990d40a65f19
Anshul Kamath <anshulkam@gmail.com>
2022-04-24 20:14:46 -0700

    implemented ec_is_inv

--- src/ec.c
+++ src/ec.c
@@ -10,3 +10,11 @@ void ec_init(ec_t *dest, const big_uint_t *a, const big_uint_t *b, const mod_t *
     dest->mod_n = mod_n;
     dest->g = g;
 }
+
+uint8_t ec_is_inv(const ec_t *ec, const point_t *p1, const point_t *p2) {
+    point_t exp_p2;
+    point_copy(&exp_p2, p1);
+
+    mod_neg(exp_p2.y, p1->y, ec->mod_p);
+    return point_equals(&exp_p2, p2);
+}
