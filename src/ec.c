7a921741b7c3a9052e8eff2e26d721648ee7f4e0
Anshul Kamath <anshulkam@gmail.com>
2022-04-25 20:08:49 -0700

    implemented ec_keygen

--- src/ec.c
+++ src/ec.c
@@ -2,7 +2,11 @@
 
 #include "big-uint.h"
 #include "mod.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define DEBUG 0
 
 void ec_init(ec_t *dest, const big_uint_t *a, const big_uint_t *b, const mod_t *mod_p, const mod_t *mod_n, const point_t *g) {
     dest->a = a;
@@ -106,4 +110,44 @@ void ec_mult(point_t *res, const big_uint_t *k, const point_t *pt, const ec_t *e
     }
 
     point_copy(res, &res_int);
+}
+
+void ec_keygen(point_t *pu_key, big_uint_t *pr_key, const ec_t *ec) {
+    #if DEBUG
+    // FOR DEBUGGING PURPOSES ONLY:
+    srand(1);
+    #endif
+
+    const uint16_t SIZE = pr_key->len;
+    const uint16_t BYTES_PER_UINT = 8;
+    
+    // use /dev/urandom to generate cryptographically-secure, random numbers
+    FILE *file = fopen("/dev/urandom", "r");
+
+    if (file == NULL) {
+        fprintf(stderr, "Error while opening /dev/urandom");
+        return;
+    }
+
+    // create an intermediate buffer for k (the private key)
+    uint32_t k_arr[SIZE];
+    uint32_t buf;
+
+    for (uint32_t i = 0; i < SIZE; i++) {
+        #if DEBUG
+        k_arr[i] = (rand() & 0xff) << 24 | (rand() & 0xff) << 16 | (rand() & 0xff) << 8 | (rand() & 0xff);
+        #else
+        if (fread(&k_arr[i], sizeof(char), BYTES_PER_UINT, file) != BYTES_PER_UINT) {
+            fprintf(stderr, "Error while generating random private key.\n");
+            return;
+        }
+        #endif
+    }
+
+    big_uint_t k;
+    big_uint_init(&k, k_arr, SIZE);
+    big_uint_copy(pr_key, &k);
+    mod_sub(&k, &k, big_uint_get_zero(), ec->mod_p);
+    
+    ec_mult(pu_key, pr_key, ec->g, ec);
 }
